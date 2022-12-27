e620792a5315ff6edf531c5b52b0ec4e86ab10cf
Aryan Jagarwal <ajbaggar@gmail.com>
2022-12-27 12:18:54 +0530

    Initial Commit

---
+++ ExpenseTrack/ExpenseTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackApp.swift
+//  ExpenseTrack
+//
+//  Created by Aryan Jagarwal on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
