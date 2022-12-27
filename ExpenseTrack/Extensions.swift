0466b4f1c38e9bf6a4b5c35ea5736f8b773d66bf
Aryan Jagarwal <ajbaggar@gmail.com>
2022-12-27 13:23:13 +0530

    done with color schemes

---
+++ ExpenseTrack/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTrack
+//
+//  Created by Aryan Jagarwal on 27/12/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
