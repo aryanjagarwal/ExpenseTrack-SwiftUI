9e54d8eccaf097c0057dcfde360df3aa9d8cf5ea
Aryan Jagarwal <ajbaggar@gmail.com>
2023-02-21 15:22:46 +0530

    transaction amount

--- ExpenseTrack/TransactionRaw.swift
+++ ExpenseTrack/TransactionRaw.swift
@@ -11,7 +11,24 @@ struct TransactionRaw: View {
     var transaction: Transaction
     var body: some View {
         HStack(spacing: 20){
-            
+            VStack(alignment: .leading, spacing: 6) {
+                // MARK: Transcation Merchant
+                Text(transaction.merchant)
+                    .font(.subheadline)
+                    .bold()
+                    .lineLimit(1)
+                
+                // MARK: Transaction Category
+                Text(transaction.category)
+                    .font(.footnote)
+                    .opacity(0.7)
+                    .lineLimit(1)
+                
+                // MARK: Transaction Date
+                Text(transaction.dateParsed, format: .dateTime.year().month().day())
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
         }
         .padding([.top, .bottom], 8)
     }
